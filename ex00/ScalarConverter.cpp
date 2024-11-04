a7b80a41a41a76721e6fecfff90d8ca38c2c4b5a
luca_di_fiori <lucadifiori@hotmail.it>
2024-11-04 19:47:45 +0100

    ex00 finito

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -20,7 +20,7 @@ int ScalarConverter::_int = 0;
 float ScalarConverter::_float = 0;
 double ScalarConverter::_double = 0;
 
-
+#include <cstdio> //DA TOGLIERE
 
 //------------------------------------------------------------------------------
 //                                 CANONICAL
@@ -63,139 +63,236 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other)
 //------------------------------------------------------------------------------
 
 //___________________________________________________________________check_utils
+/**
+ * @brief Checks if the input string represents a special floating-point value (NaN or infinity).
+ *
+ * This method determines if the given input string corresponds to a special floating-point value
+ * such as NaN (Not a Number) or infinity (positive or negative). If the input matches one of these
+ * special values, it sets the appropriate type (`NAN_CONVERSION` or `INF_CONVERSION`) and stores
+ * the input string in the class variables.
+ *
+ * @param input The input string to be checked.
+ * @return true if the input represents NaN or infinity, false otherwise.
+ */
 bool ScalarConverter::isInfNan(std::string& input)
 {
 	if (input == "nan" || input == "+nan" || input == "-nan" 
         || input == "nanf" || input == "+nanf" || input == "-nanf")
 	{
 		ScalarConverter::_type = NAN_CONVERSION;
+		ScalarConverter::_input = input;
 		return (true);
 	}
 	if (input == "inf" || input == "+inf" || input == "-inf" 
 			 || input == "inff" || input == "+inff" || input == "-inff")
 	{
 		ScalarConverter::_type = INF_CONVERSION;
+		ScalarConverter::_input = input;
 		return (true);
 	}
 	return (false);
 }
 
-bool ScalarConverter::isValid(std::string& input)
+/**
+ * @brief Checks if the input string is a single printable character.
+ *
+ * This method determines if the given input string consists of a single printable character.
+ * If the character is a digit, it sets the type to `BASIC_CONVERSION`, 
+ * otherwise it sets the type to `CHAR`.
+ * The input string is stored in the class variables.
+ *
+ * @param input The input string to be checked.
+ * @return true if the input is a single printable character, false otherwise.
+ */
+bool ScalarConverter::isSingleCharacter(std::string& input)
 {
-    int dotCount = 0;
+    if (input.length() == 1) 
+    {
+        char ch = input[0];
+        if (std::isprint(ch)) 
+        {
+            ScalarConverter::_type = std::isdigit(ch) ? BASIC_CONVERSION : CHAR;
+            ScalarConverter::_input = input;
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * @brief Checks if the input string is a valid number.
+ *
+ * This method determines if the given input string represents a valid number. 
+ * It supports optional sign characters ('+' or '-'), decimal points, and an 'f' suffix for floats.
+ * If the input is a valid number, it sets the type to `BASIC_CONVERSION` and stores the input string 
+ * in the class variables.
+ *
+ * @param input The input string to be checked.
+ * @return true if the input is a valid number, false otherwise.
+ */
+bool ScalarConverter::isValidNumber(std::string& input)
+{
     bool hasDigit = false;
-    bool hasDigitAfterDot = false;
+    bool hasDot = false;
     size_t len = input.length();
     
-    // Check if input is inf or nan
-    if (isInfNan(input)) 
-	{
-        ScalarConverter::_input = input;
-        return (true);
-    }
 
-    // Check if input is a single character
-    if (len == 1 && (isalpha(input[0]) || isdigit(input[0]))) 
-	{
-		ScalarConverter::_type = BASIC_CONVERSION;
-        ScalarConverter::_input = input;
-        return (true);
-    }
-
-    // Check if input is a valid number (optional sign, decimal, and 'f' suffix for floats)
     size_t start = (input[0] == '+' || input[0] == '-') ? 1 : 0;
 
     for (size_t i = start; i < len; i++) 
-	{
+    {
         if (input[i] == '.') 
-		{
-            dotCount++;
-            if (dotCount > 1) return false;  // Only one dot is allowed
+        {
+            if (hasDot) 
+                return (false);  // Only one decimal point is allowed
+            if (i + 1 >= len || !std::isdigit(input[i + 1]))
+                return (false);  // Must have a digit after the decimal point
+            hasDot = true;
         } 
-        else if (isdigit(input[i])) 
-		{
+        else if (std::isdigit(input[i])) 
+        {
             hasDigit = true;
-            if (dotCount > 0) 
-				hasDigitAfterDot = true;  // Set flag for digit after dot
         } 
         else if (input[i] == 'f' && i == len - 1) 
-		{
-            // 'f' suffix is valid only if it's the last character
-            continue;
+        {
+            ScalarConverter::_type = BASIC_CONVERSION;
+            ScalarConverter::_input = input;
+            return hasDigit;
         } 
-        else
-            return (false);  // Invalid character
+        else 
+            return (false);  // Invalid character found
     }
 
-    // Final validation check
-    if (!hasDigit || (dotCount == 1 && !hasDigitAfterDot)) 
-		return (false);
+    if (hasDigit) 
+    {
+        ScalarConverter::_type = BASIC_CONVERSION;
+        ScalarConverter::_input = input;
+        return (true);
+    }
+    return (false);
+}
 
-	ScalarConverter::_type = BASIC_CONVERSION;
-    ScalarConverter::_input = input;
-    return (true);
+
+bool ScalarConverter::checkInput(std::string& input)
+{
+    if (isInfNan(input)) 
+        return (true);
+
+    if (isSingleCharacter(input)) 
+        return (true);
+
+    if (isValidNumber(input)) 
+        return (true);
+
+    return (false);
 }
 
 
 //_________________________________________________________________convert_utils
-void ScalarConverter::toChar()
+/**
+ * @brief Converts the input double value to a char.
+ *
+ * This method converts the given double value to a char. If the double value is within the 
+ * printable ASCII range (32-126), it is converted to a char and stored in the class variables.
+ * The converted char is then printed to the standard output.
+ *
+ * @param input_double The double value to be converted to a char.
+ */
+void ScalarConverter::toChar(double input_double)
 {
-	//if (ScalarConverter::_type == INFNAN_DOUBLE || ScalarConverter::_type == INFNAN_FLOAT)
-	//{
-	//	std::cout << YELLOW << "char: " << RED << "Impossible" << RESET << std::endl;
-	//	return;
-	//}
-
-	long long intValue = std::stoll(ScalarConverter::_input);
-
-	if (intValue >= 32 && intValue <= 126)
+	if (input_double >= 32 && input_double <= 126)
 	{
-		ScalarConverter::_char = static_cast<char>(intValue);
+		ScalarConverter::_char = static_cast<char>(input_double);
 		ScalarConverter::printChar(true);
 	}
 	else
 		ScalarConverter::printChar(false);
-
 }
 
-void ScalarConverter::toInt()
+/**
+ * @brief Converts the input double value to an int.
+ *
+ * This method converts the given double value to an int. If the double value is within the 
+ * range of an int, it is converted to an int and stored in the class variables.
+ * The converted int is then printed to the standard output.
+ *
+ * @param input_double The double value to be converted to an int.
+ */
+void ScalarConverter::toInt(double input_double)
 {
-
-	//if (ScalarConverter::_type == INFNAN_DOUBLE || ScalarConverter::_type == INFNAN_FLOAT)
-	//{
-	//	std::cout << PURPLE << "int: " << RED << "Impossible" << RESET << std::endl;
-	//	return;
-	//}
-
-	long long intValue = std::stoll(ScalarConverter::_input);
-
-	if (intValue >= 2147483647  && intValue <= -2147483648)
+	if (input_double >= std::numeric_limits<int>::min() 
+		&& input_double <= std::numeric_limits<int>::max())
 	{
-		ScalarConverter::_int = intValue;
+		ScalarConverter::_int = static_cast<int>(input_double);
 		ScalarConverter::printInt(true);
 	}
 	else
 		ScalarConverter::printInt(false);
 }
 
+/**
+ * @brief Converts the input double value to a float.
+ *
+ * This method converts the given double value to a float. If the double value is within the 
+ * range of a float, it is converted to a float and stored in the class variables.
+ * The converted float is then printed to the standard output.
+ *
+ * @param input_double The double value to be converted to a float.
+ */
+void ScalarConverter::toFloat(double input_double) 
+{
+	if (input_double >= -std::numeric_limits<float>::max() 
+		&& input_double <= std::numeric_limits<float>::max())
+	{
+		ScalarConverter::_float = static_cast<float>(input_double);
+		ScalarConverter::printFloat(true);
+	}
+	else
+		ScalarConverter::printFloat(false);
+} 
+
+/**
+ * @brief Converts the input double value to a double.
+ *
+ * This method converts the given double value to a double. If the double value is within the 
+ * range of a double, it is stored in the class variables.
+ * The converted double is then printed to the standard output.
+ *
+ * @param input_double The double value to be converted to a double.
+ */
+void ScalarConverter::toDouble(double input_double) 
+{
+	if (input_double >= -std::numeric_limits<double>::max() 
+		&& input_double <= std::numeric_limits<double>::max())
+	{
+		ScalarConverter::_double = input_double;
+		ScalarConverter::printDouble(true);
+	}
+	else
+		ScalarConverter::printDouble(false);
+}
 
 
-
-
-//CONTROLLARLE --> misa che sono sbagliati 
-/*
-	Note: In the if statement, I check if the input is one of the special values 
-	      (regardless of whether it's float or double) because:
-		- If the input is "nan", "inf", "-inf", or "+inf", I need to convert it 
-		  to the corresponding float value using nanf() or infinity().
-		- If the input is already a special float value (like "nanf" or "inff"), 
-		  I need to convert it from a string to the actual float representation.
-
-	Note: In C++, the functions std::numeric_limits<float>::quiet_NaN() 
-	      and std::numeric_limits<double>::quiet_NaN() generate a special 
-		  floating-point value called NaN (Not-a-Number) in its "quiet" form 
-		  (i.e., without signaling an exception).
-*/
+/**
+ * @brief Converts the input to a special floating-point value (NaN or infinity) for float type.
+ *
+ * This method converts the input to a special floating-point value such as NaN (Not a Number) 
+ * or infinity (positive or negative) for the float type. If the input type is `NAN_CONVERSION`, 
+ * it sets the float value to NaN. If the input type is `INF_CONVERSION`, it sets the float value 
+ * to positive or negative infinity based on the input string.
+ *
+ * Note: In the if statement, I check if the input is one of the special values 
+ *       (regardless of whether it's float or double) because:
+ *       - If the input is "nan", "inf", "-inf", or "+inf", I need to convert it 
+ *         to the corresponding float value using nanf() or infinity().
+ *       - If the input is already a special float value (like "nanf" or "inff"), 
+ *         I need to convert it from a string to the actual float representation.
+ *
+ * Note: In C++, the functions std::numeric_limits<float>::quiet_NaN() 
+ *       and std::numeric_limits<double>::quiet_NaN() generate a special 
+ *       floating-point value called NaN (Not-a-Number) in its "quiet" form 
+ *       (i.e., without signaling an exception).
+ */
 void ScalarConverter::toFloatInfNan() 
 {
     if (ScalarConverter::_type == NAN_CONVERSION) 
@@ -209,10 +306,10 @@ void ScalarConverter::toFloatInfNan()
         else
             ScalarConverter::_float = std::numeric_limits<float>::infinity();
     }
-	std::cout << "float: " << _float << "f" << std::endl;
+	std::cout << CYAN << "float: " << RESET << _float << "f" << std::endl;
 }
 
-
+// Convert the input to an infinite or a NaN double value
 void ScalarConverter::toDoubleInfNan() 
 {
     if (ScalarConverter::_type == NAN_CONVERSION) 
@@ -226,40 +323,23 @@ void ScalarConverter::toDoubleInfNan()
         else
             ScalarConverter::_double = std::numeric_limits<double>::infinity();
 	}
-    std::cout << "double: " << _double << std::endl;
+    std::cout << GREEN << "double: " << RESET << _double << std::endl;
 }
 
 
-
-
-
-
-
-
-//MANCAQUESTOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
-/*void ScalarConverter::toFloat() 
-{
-
-
-	else 
-		{
-
-            float floatValue = std::stof(_input);
-            if (floatValue >= std::numeric_limits<float>::min() && floatValue <= std::numeric_limits<float>::max()) {
-                _float = floatValue;
-                std::cout << "float: " << std::fixed << std::setprecision(1) << _float << "f" << std::endl;
-            } else {
-                std::cout << "float: Impossible" << std::endl;
-            }
-        } 
-    }
-*/
 
 //___________________________________________________________________print_utils
 void ScalarConverter::printChar(bool isPrintable)
 {
 	if (isPrintable == true)
-		std::cout << YELLOW << "char: '" << RESET << ScalarConverter::_char << "'" << std::endl;
+	{
+		if (ScalarConverter::_type == CHAR)
+			std::cout << YELLOW << "char: '" << RESET << ScalarConverter::_input[0] 
+				<< "'"  << std::endl;
+		else
+			std::cout << YELLOW << "char: '" << RESET << ScalarConverter::_char 
+				<< "'" << std::endl;
+	}
 	else
 		std::cout << YELLOW << "char: " << RED << "Impossible" << RESET << std::endl;
 
@@ -293,41 +373,64 @@ void ScalarConverter::printDouble(bool isPrintable)
 
 
 
-
-
-
 //------------------------------------------------------------------------------
 //                               PUBLIC METHODS
 //------------------------------------------------------------------------------
-/*
-   Note: static_cast is the primary cast type you’ll use for int, float, double, 
-         and char conversions in this exercise. It’s safe, well-defined, and works 
-		 at compile time, making it ideal for numeric and character conversions.
-*/
+/**
+ * @brief Converts the input string to various scalar types (char, int, float, double).
+ *
+ * This method converts the input string to different scalar types based on the determined type.
+ * It first checks if the input is valid using `checkInput`. If the input is invalid, it prints an error message.
+ * If the input is valid, it converts the input string to a double and then performs the appropriate conversions
+ * based on the type (`CHAR`, `BASIC_CONVERSION`, `INF_CONVERSION`, or `NAN_CONVERSION`).
+ *
+ * Note: static_cast is the primary cast type you’ll use for int, float, double, 
+ *       and char conversions in this exercise. It’s safe, well-defined, and works 
+ *       at compile time, making it ideal for numeric and character conversions.
+ * 
+ * Note: The variable input_double is used to store the converted input.
+*       A double is chosen here, but you could use long or another large type.
+*       I just need a big value to store the input, and ultimately I have to
+*       convert it to double anyway.
+ *
+ * @param input The input string to be converted.
+ */
 void ScalarConverter::convert(std::string input)
 {
-	if (!ScalarConverter::isValid(input))
+	if (!ScalarConverter::checkInput(input))
 	{
 		std::cout << RED << "Invalid input" << RESET << std::endl;
 		return;
 	}
+
+
+	double input_double = atof(ScalarConverter::_input.c_str());
 	
 	switch(ScalarConverter::_type)
 	{
-		case (BASIC_CONVERSION):
-			ScalarConverter::toChar();
-			ScalarConverter::toInt();
-			ScalarConverter::toFloat();
-			ScalarConverter::toDouble();
+		case (CHAR): 
+			ScalarConverter::printChar(true);
+			ScalarConverter::_int = static_cast<int>(ScalarConverter::_input[0]);
+			ScalarConverter::_float = static_cast<float>(ScalarConverter::_input[0]);
+			ScalarConverter::_double = static_cast<double>(ScalarConverter::_input[0]);
+			ScalarConverter::printInt(true);
+			ScalarConverter::printFloat(true);
+			ScalarConverter::printDouble(true);
 			break;
-		case (INF_CONVERSION || NAN_CONVERSION):
+		case (BASIC_CONVERSION):
+			ScalarConverter::toChar(input_double);
+			ScalarConverter::toInt(input_double);
+			ScalarConverter::toFloat(input_double);
+			ScalarConverter::toDouble(input_double);
+			break;
+		case (INF_CONVERSION):
+		case (NAN_CONVERSION):
 			ScalarConverter::printChar(false);
 			ScalarConverter::printInt(false);
 			ScalarConverter::toFloatInfNan();
 			ScalarConverter::toDoubleInfNan();
 			break;
 	}
-
 
 }
 
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -28,14 +28,16 @@
 #include <iomanip>
 #include <limits>
 #include <stdexcept>
-#include <cmath> // Per std::isinf e std::isnan
+#include <cmath>
+#include <sstream>
 
 // Enum for the type of the input (used in the switch case)
 enum check
 {
+	CHAR,
+	BASIC_CONVERSION,
 	INF_CONVERSION,
-	NAN_CONVERSION,
-	BASIC_CONVERSION
+	NAN_CONVERSION
 };
 
 class ScalarConverter
@@ -48,59 +50,44 @@ class ScalarConverter
 		static std::string _input;
 		//type: the type of the input used for the switch case
 		static check _type;
-		//char: the char value of the input used for the conversion
+		//the converted values
 		static char _char;
-		//int: the int value of the input used for the conversion
 		static int _int;
-		//float: the float value of the input used for the conversion
 		static float _float;
-		//double: the double value of the input used for the conversion
 		static double _double;
+
 
 		//----------------------------------------------------------------------
 		//                               UTILS
 		//----------------------------------------------------------------------
-
 	    //___________________________________________________________check_utils
-		// Check if the input is an infinite or a NaN
-		static bool isInfNan(std::string& input); //--> se lo trovo setto _check a INFNAN
-		// Check if the input is a valid type
-		static bool isValid(std::string& input);  //-->qui controllo se il tipo è valido, se lo è inizializzo _input cosi che tutti gli altri possano prenderlo da li
-		//chiamo isInfnNan e nel caso esco, controllo se c'è piu di una lettera, se c'è una lettera sola controllare che sia o singola o una f alla fine, se c'è un punto e se c'è un segno (e ne tengo il conto), 
+		static bool isInfNan(std::string& input);
+		static bool isValidNumber(std::string& input);
+		static bool isSingleCharacter(std::string& input);
+		static bool checkInput(std::string& input);
 
 
 		//_________________________________________________________convert_utils
-		// Convert the input to a char
-		static void toChar();
-		// Convert the input to an int
-		static void toInt();             //--> Questi fanno eseguono controlli (es per gli int se è un nan o un inf o se è fuori range) e verranno chiamati tutti
-		// Convert the input to a float
-		static void toFloat();
-		// Convert the input to a double
-		static void toDouble();
+		// Convert the input to the desired type by performing the cast
+		/* Note: The variable input_double is used to store the converted input.
+		*       A double is chosen here, but you could use long or another large type.
+		*       I just need a big value to store the input, and ultimately I have to
+		*       convert it to double anyway.
+		*/	
+		static void toChar(double input_double);
+		static void toInt(double input_double);
+		static void toFloat(double input_double);
+		static void toDouble(double input_double);
 		// Convert the input to an infinite or a NaN double value
 		static void toDoubleInfNan();    
 		// Convert the input to an infinite or a NaN float value
 		static void toFloatInfNan();
 		
 	    //___________________________________________________________print_utils	
-		// Print the char
 		static void printChar(bool isPrintable);
-		// Print the int
 		static void printInt(bool isPrintable);
-		// Print the float             //--> forse questi non servono
 		static void printFloat(bool isPrintable);
-		// Print the double
 		static void printDouble(bool isPrintable);
-		/* sarebbero cose del genere
-			void ScalarConverter::printInt(int value) {
-    		std::cout << "int: " << value << std::endl;
-			}
-
-			void ScalarConverter::printFloat(float value) {
-    		std::cout << "float: " << std::fixed << std::setprecision(1) << value << "f" << std::endl;
-			}
-		*/
 
 
 	//--------------------------------------------------------------------------
@@ -116,66 +103,13 @@ class ScalarConverter
 	//--------------------------------------------------------------------------
 	//                           PUBLIC METHODS
 	//--------------------------------------------------------------------------
-		// Convert the input to the right type
 		static void convert(std::string input);
-		// Print the result
-		//static void print();
 };
 
 #endif
 
 
 
-/*
-#include "ScalarConverter.hpp"
-#include <iostream>
-#include <sstream>
-#include <iomanip>
-#include <limits>
-#include <stdexcept>
-#include <cmath> // Per std::isinf e std::isnan
-
-
-
-void ScalarConverter::toInt(const std::string& input) {
-    try {
-        if (input == "inf" || input == "+inf" || input == "-inf" || input == "nan") {
-            throw std::invalid_argument("Special floating-point value");
-        }
-        int value = std::stoi(input);
-        std::cout << "int: " << value << std::endl;
-    } catch (const std::exception& e) {
-        std::cout << "int: impossible" << std::endl;
-    }
-}
-
-void ScalarConverter::toFloat(const std::string& input) {
-    try {
-        float value = std::stof(input);
-        if (std::isinf(value)) {
-            std::cout << "float: " << (value > 0 ? "+inf" : "-inf") << "f" << std::endl;
-        } else if (std::isnan(value)) {
-            std::cout << "float: nanf" << std::endl;
-        } else {
-            std::cout << "float: " << std::fixed << std::setprecision(1) << value << "f" << std::endl;
-        }
-    } catch (const std::exception& e) {
-        std::cout << "float: impossible" << std::endl;
-    }
-}
-
-void ScalarConverter::toDouble(const std::string& input) {
-    try {
-        double value = std::stod(input);
-        if (std::isinf(value)) {
-            std::cout << "double: " << (value > 0 ? "+inf" : "-inf") << std::endl;
-        } else if (std::isnan(value)) {
-            std::cout << "double: nan" << std::endl;
-        } else {
-            std::cout << "double: " << value << std::endl;
-        }
-    } catch (const std::exception& e) {
-        std::cout << "double: impossible" << std::endl;
-    }
-}
+/*COSE DA FARE
+- SCRIVERE LE FUNZIONI TO...() CON QUELLO MESO NELL'ENUM
 */
