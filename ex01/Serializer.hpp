c965ec5d9ec176ae2739e77dd4f1bdfe07a2344b
luca_di_fiori <lucadifiori@hotmail.it>
2024-11-05 11:30:05 +0100

    ex01 finito

--- ex01/Serializer.hpp
+++ ex01/Serializer.hpp
@@ -13,11 +13,30 @@
 #ifndef SERIALIZER_HPP
 # define SERIALIZER_HPP
 
-# include <iostream>
-# include <string>
+# ifndef COLORS
+#  define COLORS
+#  define PURPLE "\033[1;35m"
+#  define GREEN "\033[1;32m"
+#  define CYAN "\033[1;36m"
+#  define RED "\033[1;31m"
+#  define YELLOW "\033[1;33m"
+#  define RESET "\033[0m"
+# endif
+
+# include <stdint.h>
+# include <cctype>
+# include "Data.hpp"
 
 class Serializer
 {
+	public:
+		Serializer();
+		Serializer(Serializer const & src);
+		~Serializer();
+		Serializer & operator=(Serializer const & rhs);
+
+		static uintptr_t serialize(Data* ptr);
+		static Data* deserialize(uintptr_t raw);
 };
 
 #endif
